d97abe5b1beb72364c88c98eaf985ecc57b0fa4e
Ryan Hendricks <ryanhendricks@gmail.com>
2018-09-14 07:45:45 -0400

    Added getters

--- src/store/contract/getters.js
+++ src/store/contract/getters.js
@@ -1,4 +1,5 @@
-/*
-export function someGetter (state) {
-}
-*/
+export const events = state => state.abi.filter(e => e.type === 'event');
+
+export const functions = state => state.abi.filter(e => e.type === 'function');
+
+export const constantFunctions = state => state.abi.filter(e => e.constant === true);
