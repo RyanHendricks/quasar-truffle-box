cc842994df1a6985e67a119cbfd3b79fbbc29040
Ryan Hendricks <RYAN.HENDRICKS@baruchmail.cuny.edu>
2018-09-18 14:18:32 -0400

    Added contract call log component to history page

--- src/components/dApp/ContractCallLog.vue
+++ src/components/dApp/ContractCallLog.vue
@@ -1,12 +1,43 @@
 <template>
-  <div/>
+  <q-list
+    no-border
+    link
+    inset-delimiter>
+    <q-list-header>History</q-list-header>
+    <q-item
+      v-for="(log, index) in callLog"
+      :key="index">
+      <q-item-side>
+        <q-item-tile
+          color="primary"
+          icon="history" />
+      </q-item-side>
+      <q-item-main>
+        <q-item-tile label>
+          <div class="uppercase">
+            {{ log.name }}
+          </div>
+        </q-item-tile>
+        <q-item-tile sublabel>{{ log.value }}</q-item-tile>
+      </q-item-main>
+    </q-item>
+  </q-list>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
-  // name: 'ComponentName',
+  name: 'ContractCallLog',
   data() {
-    return {};
+    return {
+    };
+  },
+  computed: {
+    ...mapState('contract', ['callLog']),
+  },
+  methods: {
+
   },
 };
 </script>
